49a2b997bf117eb8baefa798b647ebdf22a150e3
Marius Ilie <marius.ilie@mready.net>
2018-04-08 20:43:29 +0300

    document model & view added

--- frontend/src/app/documents/documents.component.ts
+++ frontend/src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   moduleId: module.id,
@@ -8,4 +9,27 @@ import { Component } from '@angular/core'
 
 export class DocumentsComponent {
   title = "Documents";
+  documents: Document[] = [
+    {
+      title: 'My first document',
+      description: 'Here is a great document for testing',
+      file_url: 'http://google.com/',
+      updated_at: '08/04/18',
+      image_url: 'http://google.com/'
+    },
+    {
+      title: 'My second document',
+      description: 'Here is another great document for testing',
+      file_url: 'http://google.com/',
+      updated_at: '08/04/18',
+      image_url: 'http://google.com/'
+    },
+    {
+      title: 'My last document',
+      description: 'Here is last great document for testing',
+      file_url: 'http://google.com/',
+      updated_at: '08/04/18',
+      image_url: 'http://google.com/'
+    },
+  ]
 }

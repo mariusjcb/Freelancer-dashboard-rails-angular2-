df8b5f46ffb0714af4305aecba60201b50c09e02
Marius Ilie <marius.ilie@mready.net>
2018-04-08 20:13:27 +0300

    documents page added

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'homepage',
   templateUrl: 'homepage.component.html',
   styleUrls: ['homepage.component.css']
